7e56e530b504285fcda76e5e166efe55aa313f67
Prabh Sidhu <sidhu.prabhsidhu@gmail.com>
2020-11-15 20:57:55 -0600

    Commited in Nov

--- Controllers/ProfessionalsController.cs
+++ Controllers/ProfessionalsController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using bridge_api.Models.Sql;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using bridge_api.Models.Professionals;
+using Newtonsoft.Json;
 
 namespace bridge_api.Controllers
 {
@@ -18,6 +20,16 @@ namespace bridge_api.Controllers
         {
             CategorySql _CategorySql = new CategorySql();
             return Ok(_CategorySql.GetCatFromDB());
+        }
+
+        [HttpGet]
+        [Route("search")]
+        public IActionResult SearchPro([FromQuery] string cat, [FromQuery] string city)
+        {
+            ProSql _ProSql = new ProSql();
+            List<Professional> ListReturned = _ProSql.GetProListByCategoryFromDB(cat, city);
+            string json = JsonConvert.SerializeObject(ListReturned);
+            return Ok(json);
         }
     }
 }
